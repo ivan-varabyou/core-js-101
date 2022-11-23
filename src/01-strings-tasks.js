8adb70811a3afd4d1c0a3e247befc13341da3c13
Ivan Varabyou <onebelbiz@gmail.com>
2022-11-23 17:54:17 +0300

    faet: add extractNameFromTemplate

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -65,8 +65,10 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, John Doe!' => 'John Doe'
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  return value
+    .slice(7)
+    .replace('!', '');
 }
 
 
