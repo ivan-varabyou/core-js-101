778bfd3db80a32ce4f114940dd778c2defb13220
Ivan Varabyou <onebelbiz@gmail.com>
2022-11-24 19:31:44 +0300

    feat: add findAllOccurrences

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -382,8 +382,8 @@ function getFalsyValuesCount(arr) {
  *    [ null, undefined, null ], null => 2
  *    [ true, 0, 1, 'true' ], true => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, items) {
+  return arr.filter((item) => item === items).length;
 }
 
 /**
